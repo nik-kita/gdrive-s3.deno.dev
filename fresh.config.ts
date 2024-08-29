388d9dc022f6fe64d863bdb23f52c623fe31ecbe
nik-kita <nik.deno.dev@gmail.com>
2024-08-29 19:37:45 +0200

    plugins/kv_oauth

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,6 +1,10 @@
-import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
+import { defineConfig } from "$fresh/server.ts";
+import kv_oauth from "./plugins/kv_oauth.ts";
 
 export default defineConfig({
-  plugins: [tailwind()],
+  plugins: [
+    tailwind(),
+    kv_oauth,
+  ],
 });
